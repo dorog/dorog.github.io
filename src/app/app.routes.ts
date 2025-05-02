0c112e483043e94d1963d2eab61c6afc6dd342a9
dorog <dorogi.x@gmail.com>
2025-05-02 11:17:42 +0200

    Add angular material with two empty pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { StoriesComponent } from './stories/stories.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
+  { path: 'stories', component: StoriesComponent }
+];
